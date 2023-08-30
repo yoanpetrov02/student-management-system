6d1506e039ead65b1a0799250ffe7e6a96407772
Yoan Petrov <yocito3010@gmail.com>
2023-08-30 20:52:21 +0300

    Add LoginService class.
    The class is not yet implemented as it will use JWT authentication.

---
+++ StudentManagementSystem/src/main/java/com/yoanpetrov/studentmanagementsystem/service/LoginService.java
@@ -0,0 +1,18 @@
+package com.yoanpetrov.studentmanagementsystem.service;
+
+public class LoginService {
+
+    /*private boolean checkUserExistence(String username, String password){
+        //TODO:
+        // - 1. Check if username exist in DB
+        // - 2. Check raw_password(hashed) against stored hash in DB
+        // - If true call createJWTToken
+        // else return 401
+    }
+    private void setJWTAsCookie(){
+        JwtService service = new JwtService();
+        service.generateToken()
+
+    }*/
+
+}
