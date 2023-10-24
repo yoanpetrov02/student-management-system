4cb4380879271c8ad804848ada27f70c6dbee99d
Yoan Petrov <yocito3010@gmail.com>
2023-10-24 10:25:07 +0300

    Document the JWT filter and service.

--- StudentManagementSystem/src/main/java/com/yoanpetrov/studentmanagementsystem/security/jwt/JwtRequestFilter.java
+++ StudentManagementSystem/src/main/java/com/yoanpetrov/studentmanagementsystem/security/jwt/JwtRequestFilter.java
@@ -16,6 +16,12 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+// TODO: 24-Oct-23 Add a case where the user is already validated.
+// TODO: 24-Oct-23 Decompose the filter method into smaller methods.
+// TODO: 24-Oct-23 Fix any faulty logic.
+/**
+ * A JWT authentication filter, executed on every incoming request.
+ */
 @RequiredArgsConstructor
 @Component
 public class JwtRequestFilter extends OncePerRequestFilter {
@@ -25,6 +31,15 @@ public class JwtRequestFilter extends OncePerRequestFilter {
     private final UserDetailsService userDetailsService;
     private final JwtService jwtService;
 
+    /**
+     * Validates the JWT token and sets the authentication on the {@code SecurityContext} on successful validation.
+     *
+     * @param request the http request.
+     * @param response the http response.
+     * @param filterChain the filter chain.
+     * @throws ServletException if an error occurs in the servlet.
+     * @throws IOException if an i/o error occurs.
+     */
     @Override
     protected void doFilterInternal(
         @NonNull HttpServletRequest request,
@@ -49,6 +64,12 @@ public class JwtRequestFilter extends OncePerRequestFilter {
         }
     }
 
+    /**
+     * Validates the Authorization header.
+     *
+     * @param authHeader the header.
+     * @return true if the header contains a Bearer token, false if it's null or contains something else.
+     */
     private boolean validateAuthHeader(String authHeader) {
         return authHeader != null && authHeader.startsWith("Bearer ");
     }
--- StudentManagementSystem/src/main/java/com/yoanpetrov/studentmanagementsystem/security/jwt/JwtService.java
+++ StudentManagementSystem/src/main/java/com/yoanpetrov/studentmanagementsystem/security/jwt/JwtService.java
@@ -15,7 +15,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
-// TODO: 19-Oct-23 Add secret key, expiration time and refresh token expiration time to application.yml
+// TODO: 24-Oct-23 Set the expiration times of the JWT tokens to normal values.
+// TODO: 24-Oct-23 Change validation logic to use the Jwts library to verify the tokens.
+/**
+ * JWT service. Used to extract different claims from a JWT token or validate it.
+ */
 @Service
 public class JwtService {
 
@@ -26,16 +30,37 @@ public class JwtService {
     @Value("${application.security.jwt.refresh-token.expiration}")
     private long refreshExpiration;
 
+    /**
+     * Validates a JWT token.
+     *
+     * @param token the token.
+     * @param userDetails the user to validate the token against.
+     * @return true if the token is not expired and represents the same user, false otherwise.
+     */
     public boolean isTokenValid(String token, UserDetails userDetails) {
         final String username = extractUsername(token);
         return username.equals(userDetails.getUsername())
             && !isTokenExpired(token);
     }
 
+    /**
+     * Extracts the username from a JWT token.
+     *
+     * @param token the token.
+     * @return the extracted username.
+     */
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
 
+    /**
+     * Extract a claim from the JWT token, using the given claim resolver.
+     *
+     * @param token the token.
+     * @param claimsResolver the resolver for the needed claim.
+     * @return the extracted claim.
+     * @param <T> the type of the claim, part of the resolver.
+     */
     public <T> T extractClaim(
         String token,
         Function<Claims, T> claimsResolver)
@@ -44,10 +69,23 @@ public class JwtService {
         return claimsResolver.apply(claims);
     }
 
+    /**
+     * Generates a JWT token for the given user.
+     *
+     * @param userDetails the user to generate a JWT token for.
+     * @return the generated JWT token.
+     */
     public String generateToken(UserDetails userDetails) {
         return generateToken(new HashMap<>(), userDetails);
     }
 
+    /**
+     * Generates a JWT token for the given user, with the given extra claims to be included in it.
+     *
+     * @param extraClaims the extra claims to include in the token.
+     * @param userDetails the user to generate a JWT token for.
+     * @return the generated JWT token.
+     */
     public String generateToken(
         Map<String, Object> extraClaims,
         UserDetails userDetails)
@@ -55,18 +93,42 @@ public class JwtService {
         return buildToken(extraClaims, userDetails, jwtExpiration);
     }
 
+    /**
+     * Generates a refresh token for the given user.
+     *
+     * @param userDetails the user to generate a refresh token for.
+     * @return the generated refresh token.
+     */
     public String generateRefreshToken(UserDetails userDetails) {
         return buildToken(new HashMap<>(), userDetails, refreshExpiration);
     }
 
+    /**
+     * Checks whether a JWT token has expired.
+     *
+     * @param token the token.
+     * @return true if the token's expiration time was before the current time, false otherwise.
+     */
     private boolean isTokenExpired(String token) {
         return extractExpiration(token).before(new Date());
     }
 
+    /**
+     * Extracts the expiration time of a JWT token.
+     *
+     * @param token the token.
+     * @return the {@code Date} object, representing the exact time of expiration.
+     */
     private Date extractExpiration(String token) {
         return extractClaim(token, Claims::getExpiration);
     }
 
+    /**
+     * Extracts all the claims from a JWT token.
+     *
+     * @param token the token.
+     * @return the extracted {@code Claims}.
+     */
     private Claims extractAllClaims(String token) {
         return Jwts
             .parserBuilder()
@@ -76,6 +138,14 @@ public class JwtService {
             .getBody();
     }
 
+    /**
+     * Builds a JWT token.
+     *
+     * @param extraClaims the extra claims to include in the token.
+     * @param userDetails the user to build the token for.
+     * @param expiration the expiration time of the token.
+     * @return the newly built JWT token.
+     */
     private String buildToken(
         Map<String, Object> extraClaims,
         UserDetails userDetails,
@@ -92,6 +162,11 @@ public class JwtService {
 
     }
 
+    /**
+     * Gets the signing key for the JWT tokens.
+     *
+     * @return the newly created {@code Key}.
+     */
     private Key getSigningKey() {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
