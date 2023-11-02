5dfb584c7c264fdadf39870026cc9fb40288c4d2
Yoan Petrov <yocito3010@gmail.com>
2023-11-02 14:53:19 +0200

    Add /refresh-token endpoint.

--- StudentManagementSystem/src/main/java/com/yoanpetrov/studentmanagementsystem/rest/controllers/AuthenticationController.java
+++ StudentManagementSystem/src/main/java/com/yoanpetrov/studentmanagementsystem/rest/controllers/AuthenticationController.java
@@ -4,6 +4,8 @@ import com.yoanpetrov.studentmanagementsystem.exceptions.ResourceNotFoundExcepti
 import com.yoanpetrov.studentmanagementsystem.rest.dto.UserAccountDto;
 import com.yoanpetrov.studentmanagementsystem.security.AuthenticationResponse;
 import com.yoanpetrov.studentmanagementsystem.services.AuthenticationService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,17 +17,33 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Rest controller for the login endpoints.
- */
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/login")
-public class LoginController {
+@RequestMapping("api/v1")
+public class AuthenticationController {
 
-    private static final Logger LOG = LoggerFactory.getLogger(LoginController.class);
+    private static final Logger LOG = LoggerFactory.getLogger(AuthenticationController.class);
 
     private final AuthenticationService authenticationService;
+
+    /**
+     * Registers the given user.
+     *
+     * @param userDto the credentials of the user's account.
+     * @return 200 and a JWT token if the user was successfully registered,
+     * 409 if a user account with this username already exists.
+     */
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> registerUser(@RequestBody UserAccountDto userDto) {
+        LOG.debug("Attempting to register user account");
+        if (authenticationService.checkUserExistence(userDto.getUsername())) {
+            LOG.debug("Account with the same username already exists, returning 409");
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+        AuthenticationResponse response = authenticationService.registerAccount(userDto);
+        LOG.debug("Successful registration, generated token is {}", response.getAccessToken());
+        return ResponseEntity.ok(response);
+    }
 
     /**
      * Authenticates the given user.
@@ -35,7 +53,7 @@ public class LoginController {
      * 404 if the user account doesn't exist,
      * 401 if the credentials weren't correct.
      */
-    @PostMapping
+    @PostMapping("/login")
     public ResponseEntity<?> authenticateUser(@RequestBody UserAccountDto userDto) {
         LOG.debug("Attempting to authenticate user account: {}", userDto.getUsername());
         try {
@@ -47,6 +65,17 @@ public class LoginController {
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         } catch (ResourceNotFoundException e) {
             LOG.debug("User not found, returning 404");
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/refresh-token")
+    public ResponseEntity<?> refreshToken(
+        HttpServletRequest request
+    ) {
+        try {
+            return ResponseEntity.ok(authenticationService.refreshToken(request));
+        } catch (ResourceNotFoundException e) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
