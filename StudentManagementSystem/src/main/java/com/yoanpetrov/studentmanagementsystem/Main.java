60f811cd667c8dc1b50cbc0f987cea0ae3946529
Yoan Petrov <yocito3010@gmail.com>
2023-11-04 16:47:51 +0200

    Remove completed TODOs.

--- StudentManagementSystem/src/main/java/com/yoanpetrov/studentmanagementsystem/Main.java
+++ StudentManagementSystem/src/main/java/com/yoanpetrov/studentmanagementsystem/Main.java
@@ -4,8 +4,6 @@ import org.apache.log4j.BasicConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// TODO: 16-Oct-23 Add JSON validation using Hibernate Validator
-// TODO: 02-Nov-23 Finish the authorization logic - add method authorization and stuff and change the http builder filtering to require roles for the different endpoints.
 // TODO: 04-Nov-23 Change POST and DELETE requests to /users/{id}/courses and /courses/{id}/users to use a path variable and work like /users/{userId}/courses/{courseId} and /courses/{courseId}/users/{userId} respectively. This way, you don't have to pass the whole user or course in JSON but rather just its id and if it's not found in the database, 404 will be returned, which is a cleaner solution.
 
 /**
