b7e6a4809ddfb32c1d6f1a0ec926af2425ea77d9
Yoan Petrov <yocito3010@gmail.com>
2023-11-08 00:21:23 +0200

    Add exception handler for MethodArgumentNotValidException.
    The handler gathers all the errors in a map and constructs a ValidationErrorBody object as the response body.

--- StudentManagementSystem/src/main/java/com/yoanpetrov/studentmanagementsystem/exceptions/ErrorBody.java
+++ StudentManagementSystem/src/main/java/com/yoanpetrov/studentmanagementsystem/exceptions/ErrorBody.java
@@ -1,12 +1,14 @@
 package com.yoanpetrov.studentmanagementsystem.exceptions;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Builder
 public class ErrorBody {
 
     private String status;
