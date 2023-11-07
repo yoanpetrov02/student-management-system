0983ae1cd95dd2cf540a23e1b392e2cd14efe0b1
Yoan Petrov <yocito3010@gmail.com>
2023-11-07 17:38:32 +0200

    Set the accounts endpoints to require an ADMIN role.

--- StudentManagementSystem/src/main/java/com/yoanpetrov/studentmanagementsystem/configurations/SecurityConfiguration.java
+++ StudentManagementSystem/src/main/java/com/yoanpetrov/studentmanagementsystem/configurations/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -63,6 +64,7 @@ public class SecurityConfiguration {
                     .requestMatchers(HttpMethod.POST, "/api/v1/courses").hasAnyRole(ADMIN.name())
                     .requestMatchers(HttpMethod.DELETE, "/api/v1/courses").hasAnyRole(ADMIN.name())
                     .requestMatchers(HttpMethod.DELETE, "/api/v1/courses/{id}").hasAnyRole(ADMIN.name())
+                    .requestMatchers("/api/v1/accounts").hasAnyRole(ADMIN.name())
                     .anyRequest().authenticated())
             .csrf(AbstractHttpConfigurer::disable)
             .sessionManagement(c ->
