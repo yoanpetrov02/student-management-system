dc586fff4411488de96f02c392e56e6367fdfde1
Yoan Petrov <yocito3010@gmail.com>
2023-11-07 23:21:56 +0200

    Add UserDto bean.

---
+++ StudentManagementSystem/src/main/java/com/yoanpetrov/studentmanagementsystem/rest/dto/UserDto.java
@@ -0,0 +1,20 @@
+package com.yoanpetrov.studentmanagementsystem.rest.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class UserDto {
+    @NotBlank(message = "First name cannot be blank")
+    private String firstName;
+    @NotBlank(message = "Last name cannot be blank")
+    private String lastName;
+    @NotBlank(message = "Email cannot be blank")
+    private String email;
+}
