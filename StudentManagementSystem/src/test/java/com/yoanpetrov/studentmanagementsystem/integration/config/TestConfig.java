fe5e8ff200f903519af54521abe375d41b25672e
Yoan Petrov <yocito3010@gmail.com>
2023-11-13 14:51:08 +0200

    Clean up unused imports.

--- StudentManagementSystem/src/test/java/com/yoanpetrov/studentmanagementsystem/integration/config/TestConfig.java
+++ StudentManagementSystem/src/test/java/com/yoanpetrov/studentmanagementsystem/integration/config/TestConfig.java
@@ -8,8 +8,6 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * Disables security for the tests.
