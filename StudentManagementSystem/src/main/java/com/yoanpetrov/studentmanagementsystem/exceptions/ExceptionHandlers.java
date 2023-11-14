98b99e4197a9ee8c8298058cb02f841dc810e791
Yoan Petrov <yocito3010@gmail.com>
2023-11-14 17:16:17 +0200

    Add AccessDeniedException handling.
    The handling of this exception allows for custom JSON to be returned whenever the exception is caught.

--- StudentManagementSystem/src/main/java/com/yoanpetrov/studentmanagementsystem/exceptions/ExceptionHandlers.java
+++ StudentManagementSystem/src/main/java/com/yoanpetrov/studentmanagementsystem/exceptions/ExceptionHandlers.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -24,19 +25,37 @@ public class ExceptionHandlers extends ResponseEntityExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<?> handleResourceNotFoundException(Throwable ex) {
         log.debug("ResourceNotFoundException, returning 404. Message: {}", ex.getMessage());
-        return new ResponseEntity<>(new ErrorBody("404", ex.getMessage()), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(new ErrorBody(
+            "404",
+            ex.getMessage()),
+            HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(ResourceConflictException.class)
     public ResponseEntity<?> handleResourceConflictException(Throwable ex) {
         log.debug("ResourceConflictException, returning 409. Message: {}", ex.getMessage());
-        return new ResponseEntity<>(new ErrorBody("409", ex.getMessage()), HttpStatus.CONFLICT);
+        return new ResponseEntity<>(new ErrorBody(
+            "409",
+            ex.getMessage()),
+            HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(BadCredentialsException.class)
     public ResponseEntity<?> handleBadCredentialsException(Throwable ex) {
         log.debug("BadCredentialsException, returning 401. Message: {}", ex.getMessage());
-        return new ResponseEntity<>(new ErrorBody("401", ex.getMessage()), HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(new ErrorBody(
+            "401",
+            ex.getMessage()),
+            HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<?> handleAccessDeniedException(Throwable ex) {
+        log.debug("AccessDeniedException, returning 401. Message: {}", ex.getMessage());
+        return new ResponseEntity<>(new ErrorBody(
+            "401",
+            "You are not authorized to access this endpoint"),
+            HttpStatus.UNAUTHORIZED);
     }
 
     @Override
