975a5a4ce8d9bbb25b84f2f6bf436cb47ac79e30
Yoan Petrov <yocito3010@gmail.com>
2023-11-14 16:36:36 +0200

    Enable method security.

--- StudentManagementSystem/src/main/java/com/yoanpetrov/studentmanagementsystem/configurations/SecurityConfiguration.java
+++ StudentManagementSystem/src/main/java/com/yoanpetrov/studentmanagementsystem/configurations/SecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -26,6 +27,7 @@ import static com.yoanpetrov.studentmanagementsystem.security.Role.*;
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfiguration {
 
     private static final String[] WHITELISTED_ENDPOINTS = {
