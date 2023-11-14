c1c047ac600f696a908423153b478e6915df1a93
Yoan Petrov <yocito3010@gmail.com>
2023-11-14 16:36:47 +0200

    Secure AccountController.

--- StudentManagementSystem/src/main/java/com/yoanpetrov/studentmanagementsystem/rest/controllers/AccountController.java
+++ StudentManagementSystem/src/main/java/com/yoanpetrov/studentmanagementsystem/rest/controllers/AccountController.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,6 +19,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/accounts")
+@PreAuthorize("hasRole('ADMIN')")
 public class AccountController {
 
     private static final Logger LOG = LoggerFactory.getLogger(AccountController.class);
